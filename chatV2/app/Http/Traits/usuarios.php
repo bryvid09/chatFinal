be1d2525c10ba43581194cf1788d25139e44b87d
bryvid09 <75569223+bryvid09@users.noreply.github.com>
2025-06-19 01:41:46 +0200

    Fix file storage paths

--- chatV2/app/Http/Traits/usuarios.php
+++ chatV2/app/Http/Traits/usuarios.php
@@ -15,11 +15,11 @@ trait usuarios
     public static function comprobarUser($datos)
     {
         //Si no existe el archivo "usuarios.json" lo crea junto con el primer usuario
-        if (!file_exists(storage_path('\app\public\usuarios.json'))) {
+        if (!file_exists(storage_path('app/public/usuarios.json'))) {
             array_push(self::$usuarios, $datos);
         } else {
             $insertar = true;
-            self::$usuarios = json_decode(file_get_contents(storage_path('\app\public\usuarios.json')));
+            self::$usuarios = json_decode(file_get_contents(storage_path('app/public/usuarios.json')));
 
             //recorre el array de objetos
             foreach (self::$usuarios as $usuarios) {
@@ -57,7 +57,7 @@ trait usuarios
         /**volvemos a insertar el array en el archivo "usuarios.json" y devolvemos
          *  un true para permitir el acceso al controlador         
          **/
-        file_put_contents(storage_path('\app\public\usuarios.json'), json_encode(self::$usuarios));
+        file_put_contents(storage_path('app/public/usuarios.json'), json_encode(self::$usuarios));
         return true;
     }
 
@@ -65,11 +65,11 @@ trait usuarios
     {
         $fecha = date("d/m/Y");
         $hora = date("H:i:s");
-        if (!file_exists(storage_path('\app\public\historial.json'))) {
+        if (!file_exists(storage_path('app/public/historial.json'))) {
             self::$conversaciones[$fecha] = array($hora => array($_SESSION['user'] => array($mensaje['mensaje'])));
-            file_put_contents(storage_path('\app\public\historial.json'), json_encode(self::$conversaciones));
+            file_put_contents(storage_path('app/public/historial.json'), json_encode(self::$conversaciones));
         } else {
-            self::$conversaciones = json_decode(file_get_contents(storage_path('\app\public\historial.json')), true);
+            self::$conversaciones = json_decode(file_get_contents(storage_path('app/public/historial.json')), true);
             if (array_key_exists($fecha, self::$conversaciones)) {
                 if (array_key_exists($hora, self::$conversaciones[$fecha])) {
                     if (array_key_exists($_SESSION['user'], self::$conversaciones[$fecha][$hora])) {
@@ -83,14 +83,14 @@ trait usuarios
             } else {
                 self::$conversaciones[$fecha] = array($hora => array($_SESSION['user'] => array($mensaje['mensaje'])));
             }
-            file_put_contents(storage_path('\app\public\historial.json'), json_encode(self::$conversaciones));
+            file_put_contents(storage_path('app/public/historial.json'), json_encode(self::$conversaciones));
         }
     }
 
     public static function leerMensajes()
     {
-        if (file_exists(storage_path('\app\public\historial.json'))) {
-            self::$conversaciones = json_decode(file_get_contents(storage_path('\app\public\historial.json')), true);
+        if (file_exists(storage_path('app/public/historial.json'))) {
+            self::$conversaciones = json_decode(file_get_contents(storage_path('app/public/historial.json')), true);
         } else {
             self::$conversaciones = [];
         }
@@ -99,8 +99,8 @@ trait usuarios
 
     public static function listaUsuarios()
     {
-        if (file_exists(storage_path('\app\public\usuarios.json'))) {
-            self::$users = json_decode(file_get_contents(storage_path('\app\public\usuarios.json')));
+        if (file_exists(storage_path('app/public/usuarios.json'))) {
+            self::$users = json_decode(file_get_contents(storage_path('app/public/usuarios.json')));
         } else {
             self::$users = [];
         }
@@ -113,8 +113,8 @@ trait usuarios
         $hora = date("H:i:s");
         $userConver = [];
 
-        if (file_exists(storage_path('\app\public\\') . $_SESSION['user'] . '.json')) {
-            $userConver = json_decode(file_get_contents((storage_path('\app\public\\') . $_SESSION['user'] . '.json')), true);
+        if (file_exists(storage_path('app/public/') . $_SESSION['user'] . '.json')) {
+            $userConver = json_decode(file_get_contents((storage_path('app/public/') . $_SESSION['user'] . '.json')), true);
             if (array_key_exists($user, $userConver)) {
                 if (array_key_exists($fecha, $userConver[$user])) {
                     if (array_key_exists($hora, $userConver[$user][$fecha])) {
@@ -132,10 +132,10 @@ trait usuarios
             $userConver = array($user => array($fecha => array($hora => array($_SESSION['user'] => $mensaje['mensaje']))));
         }
 
-        file_put_contents(storage_path('\app\public\\') . $_SESSION['user'] . '.json', json_encode($userConver));
+        file_put_contents(storage_path('app/public/') . $_SESSION['user'] . '.json', json_encode($userConver));
 
-        if (file_exists(storage_path('\app\public\\') . $user . '.json')) {
-            $userConver = json_decode(file_get_contents((storage_path('\app\public\\') . $user . '.json')), true);
+        if (file_exists(storage_path('app/public/') . $user . '.json')) {
+            $userConver = json_decode(file_get_contents((storage_path('app/public/') . $user . '.json')), true);
             if (array_key_exists($_SESSION['user'], $userConver)) {
                 if (array_key_exists($fecha, $userConver[$_SESSION['user']])) {
                     if (array_key_exists($hora, $userConver[$_SESSION['user']][$fecha])) {
@@ -153,13 +153,13 @@ trait usuarios
             $userConver = array($_SESSION['user'] => array($fecha => array($hora => array($_SESSION['user'] => $mensaje['mensaje']))));
         }
 
-        file_put_contents(storage_path('\app\public\\') . $user . '.json', json_encode($userConver));
+        file_put_contents(storage_path('app/public/') . $user . '.json', json_encode($userConver));
     }
 
     public static function consultarPrivado($user)
     {
-        if (file_exists(storage_path('\app\public\\') . $_SESSION['user'] . '.json')) {
-            $converPrivada = json_decode(file_get_contents(storage_path('\app\public\\') . $_SESSION['user'] . '.json'), true);
+        if (file_exists(storage_path('app/public/') . $_SESSION['user'] . '.json')) {
+            $converPrivada = json_decode(file_get_contents(storage_path('app/public/') . $_SESSION['user'] . '.json'), true);
             if (array_key_exists($user, $converPrivada)) {
                 return $converPrivada[$user];
             }
@@ -170,14 +170,14 @@ trait usuarios
 
     public static function desconectar()
     {
-        if (file_exists(storage_path('\app\public\usuarios.json'))) {
-            $users = json_decode(file_get_contents(storage_path('\app\public\usuarios.json')));
+        if (file_exists(storage_path('app/public/usuarios.json'))) {
+            $users = json_decode(file_get_contents(storage_path('app/public/usuarios.json')));
             foreach ($users as $user) {
                 if ($user->user == $_SESSION['user']) {
                     $user->online = 'no';
                 }
             }
-            file_put_contents(storage_path('\app\public\usuarios.json'), json_encode($users));
+            file_put_contents(storage_path('app/public/usuarios.json'), json_encode($users));
         }
     }
 }
